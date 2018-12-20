bfd9356f92c9939eccf06ef309614f4ea3554d0e
ming.li <lm_20150826@163.com>
2018-12-20 23:44:32 +0800

    集成eureka

--- eureka-server/src/test/java/com/lm/eureka/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/lm/eureka/EurekaServerApplicationTests.java
@@ -7,11 +7,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaServiceApplicationTests {
+public class EurekaServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
 
