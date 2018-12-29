a2a46beafeda275e49afb2ba7a0bcd5ef59f3aa9
ming.li <lm_20150826@163.com>
2018-12-29 17:40:34 +0800

    ribbon脱离eureka

--- microservice-consumer-movie-without-eureka/src/main/java/com/lm/consumer/controller/MovieController.java
+++ microservice-consumer-movie-without-eureka/src/main/java/com/lm/consumer/controller/MovieController.java
@@ -1,15 +1,12 @@
 package com.lm.consumer.controller;
 
-import com.lm.consumer.user.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author lming.41032@gmail.com
@@ -21,29 +18,12 @@ public class MovieController {
     private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
 
     @Autowired
-    private RestTemplate template;
-
-    @Autowired
     private LoadBalancerClient balancerClient;
 
 
-   /* @GetMapping("user/{id}")
-    public User findUserById(@PathVariable("id") String id) {
-        User user = template.getForObject("http://MICROSERVICE-PROVIDER-USER/user/" + id, User.class);
-        return user;
-    }
-
-    @GetMapping("user/id/{id}")
-    public User test(@PathVariable("id") String id) {
-        ServiceInstance serviceInstance = balancerClient.choose("microservice-provider-user");
-        final String url = serviceInstance.getUri() + "/user/" + id;
-        User user = template.getForObject(url, User.class);
-        return user;
-    }*/
-
     @GetMapping("/log-instance")
     public void logUserInstance() {
-        ServiceInstance serviceInstance = balancerClient.choose("microservice-provider-user");
+        ServiceInstance serviceInstance = balancerClient.choose("provider-user");
         LOGGER.info("{},{},{},{}", serviceInstance.getServiceId(),
                 serviceInstance.getHost()
                 , serviceInstance.getPort()
