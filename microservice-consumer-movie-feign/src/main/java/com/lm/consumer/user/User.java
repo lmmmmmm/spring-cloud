2cfb7512b98e59887e8acd56a5451ca9e1e13d72
ming.li <lm_20150826@163.com>
2018-12-30 23:51:52 +0800

    feign

---
+++ microservice-consumer-movie-feign/src/main/java/com/lm/consumer/user/User.java
@@ -0,0 +1,91 @@
+package com.lm.consumer.user;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author lming.41032@gmail.com
+ * @date 18-12-21 下午5:20
+ */
+public class User implements Serializable {
+
+    private String id;
+
+    private String name;
+
+    private String password;
+
+    private String telphone;
+
+    private String email;
+
+    private Integer sex;
+
+    private Date gmtCreate;
+
+    private Date gmtUpdate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelphone() {
+        return telphone;
+    }
+
+    public void setTelphone(String telphone) {
+        this.telphone = telphone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Date getGmtUpdate() {
+        return gmtUpdate;
+    }
+
+    public void setGmtUpdate(Date gmtUpdate) {
+        this.gmtUpdate = gmtUpdate;
+    }
+}
